9ceb2524f8a84ba23997ed17e9a0727c383d6419
chenpiqian <237053176@qq.com>
2018-07-19 15:38:58 +0800

    adapter

--- src/main/java/com/cpq/testvalidate/service/ValidateInterfaceImpl.java
+++ src/main/java/com/cpq/testvalidate/service/ValidateInterfaceImpl.java
@@ -3,7 +3,7 @@ package com.cpq.testvalidate.service;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.Feature;
-import com.github.codingsoldier.paramsvalidate.ValidateInterface;
+import com.github.codingsoldier.paramsvalidate.ValidateInterfaceAdapter;
 import com.github.codingsoldier.paramsvalidate.ValidateUtils;
 import com.github.codingsoldier.paramsvalidate.bean.Parser;
 import com.github.codingsoldier.paramsvalidate.bean.ResultValidate;
@@ -20,7 +20,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Component
-public class ValidateInterfaceImpl implements ValidateInterface, InitializingBean {
+public class ValidateInterfaceImpl extends ValidateInterfaceAdapter implements  InitializingBean {
 
     //不使用缓存
     @Autowired
@@ -43,48 +43,46 @@ public class ValidateInterfaceImpl implements ValidateInterface, InitializingBea
 
     /**
      * json解析器
-     * 1、使用默认解析器jackson，直接返回null即可
+     * 1、使用默认解析器jackson，可不覆盖此方法
      * 2、使用gson，请返回 new Parser(Gson.class);
      * 3、使用fastjson，请返回new Parser(JSON.class, Feature[].class)
      * 为了支持fastjson，搞得好坑爹
      */
     public Parser getParser(){
-        //return null;
         //return new Parser(Gson.class);
         return new Parser(JSON.class, Feature[].class);
     }
 
-    ////不使用缓存，返回空map即可
-    //@Override
-    //public Map<String, Object> getCache(ValidateConfig validateConfig) {
-    //    return new HashMap<>();
-    //}
-    //
-    ////不使用缓存，本方法可不处理
-    //@Override
-    //public void setCache(ValidateConfig validateConfig, Map<String, Object> json) {
-    //
-    //}
-    //
-    ////不使用缓存，可以不实现InitializingBean，不必实现本方法
-    //@Override
-    //public void afterPropertiesSet() throws Exception {
-    //
-    //}
-
-
-    //获取redis缓存中的校验规则
+    /**
+     * 不使用缓存，可不覆盖此方法
+     * 获取redis缓存中的校验规则
+     */
     @Override
     public Map<String, Object> getCache(ValidateConfig validateConfig) {
         String key = createKey(validateConfig);
         return redisTemplate.opsForHash().entries(key);
     }
 
-    //设置redis校验规则到缓存中
+    /**
+     * 不使用缓存，可不覆盖此方法
+     * 设置redis校验规则到缓存中
+     */
     @Override
     public void setCache(ValidateConfig validateConfig, Map<String, Object> json) {
         String key = createKey(validateConfig);
         redisTemplate.opsForHash().putAll(key, json);
+    }
+
+
+    //创建缓存key
+    private String createKey(ValidateConfig validateConfig){
+        String basePath = ValidateUtils.trimBeginEndChar(basePath(), '/') + "/";
+        String fileName = validateConfig.getFile().substring(0, validateConfig.getFile().lastIndexOf(".json"));
+        fileName = ValidateUtils.trimBeginEndChar(fileName, '/');
+        String jsonKey = validateConfig.getKeyName();
+        jsonKey = ValidateUtils.isBlank(jsonKey) ? jsonKey : (":"+jsonKey);
+        String temp = basePath + fileName + jsonKey;
+        return temp.replaceAll("[\\/\\-]",":");
     }
 
     //项目启动时，删除redis缓存校验规则
@@ -101,15 +99,5 @@ public class ValidateInterfaceImpl implements ValidateInterface, InitializingBea
         es.shutdown();
     }
 
-    //创建缓存key
-    private String createKey(ValidateConfig validateConfig){
-        String basePath = ValidateUtils.trimBeginEndChar(basePath(), '/') + "/";
-        String fileName = validateConfig.getFile().substring(0, validateConfig.getFile().lastIndexOf(".json"));
-        fileName = ValidateUtils.trimBeginEndChar(fileName, '/');
-        String jsonKey = validateConfig.getKeyName();
-        jsonKey = ValidateUtils.isBlank(jsonKey) ? jsonKey : (":"+jsonKey);
-        String temp = basePath + fileName + jsonKey;
-        return temp.replaceAll("[\\/\\-]",":");
-    }
 
 }
